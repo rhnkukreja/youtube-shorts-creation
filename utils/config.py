d970c4f2b8822a5f4dd40e50c3da2a4211d5a8b9
rhnkukreja <94891842+rhnkukreja@users.noreply.github.com>
2024-06-03 15:08:25 +0530

    Delete utils directory

--- utils/config.py
+++
@@ -1,9 +0,0 @@
-FONT_SIZE=80
-VIDEO_ASPECT_RATIO=(1080, 1920)
-AZURE_AI_SERVICES_KEY: str = "" # Please get yours from Azure portal
-AZURE_SPEECH_REGION: str = "" # Please get yours from Azure portal
-AZURE_ARIA: str = "en-US-AriaNeural"
-AZURE_ARIA1: str = "hi-IN-MadhurNeural"
-AZURE_US_FEMALE_MODEL: str = "en-US-SwaraNeural" # Feel free to change the voice model as per your preference
-OPENAI_API_KEY="" # Please get yours from OPENAI website
-
