b85581a4bb7a257de09dba096480cdd500351dc2
rhnkukreja <94891842+rhnkukreja@users.noreply.github.com>
2024-06-03 15:13:02 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+FONT_SIZE=80
+VIDEO_ASPECT_RATIO=(1080, 1920)
+AZURE_AI_SERVICES_KEY: str = "" # Please get yours from Azure portal
+AZURE_SPEECH_REGION: str = "" # Please get yours from Azure portal
+AZURE_ARIA: str = "en-US-AriaNeural"
+AZURE_ARIA1: str = "hi-IN-MadhurNeural"
+AZURE_US_FEMALE_MODEL: str = "en-US-SwaraNeural" # Feel free to change the voice model as per your preference
+OPENAI_API_KEY="" # Please get yours from OPENAI website
+PROJECT_PATH="" # Please enter the root directory of your project here
+BING_IMAGE_SEARCH_URL: str = "https://api.bing.microsoft.com/v7.0/images/search"
+BING_IMAGE_SEARCH_KEY="987ff52bec2e4f5b938ec859fe0c5b1e"
+IMAGEMAGICK_BINARY_PATH=" " # Please enter the path to your imagemagick installation
